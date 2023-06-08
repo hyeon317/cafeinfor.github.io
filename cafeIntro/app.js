b42725aa0e2a6976a93dc2d8036b7757258c3a79
수현 <101097019+hyeon317@users.noreply.github.com>
2023-06-08 11:25:22 +0900

    Delete cafeIntro directory

--- cafeIntro/app.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $("#bthome").bind('click', function () {
-    $("html, body").animate({scrollTop: '0'}, 500);
-  });
-
-  $('#bthome').on('click', function () {
-    $('html').scrollTop('0');
-  });
-});
